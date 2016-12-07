a63d132dab7b44d21718020df61dc92c52662fc2
Joanne Bruno <jnbruno2015@gmail.com>
2016-12-07 01:35:36 -0600

    UI styling changes and medidata addition

--- scripts/controllers/DeveloperController.js
+++ scripts/controllers/DeveloperController.js
@@ -1,32 +1,31 @@
 (function() {
-	angular
-		.module('portfolioApp')
-		.controller('DeveloperController', ['$scope', '$location', '$anchorScroll',
-			function($scope, $location, $anchorScroll) {
-				// initialize parallax
-                $('.parallax').parallax();
-				
-                //initialize side nav
-                $(".button-collapse").sideNav();
-                
-				// set page class
-				$scope.pageClass = 'developer';
+        angular
+            .module('portfolioApp')
+            .controller('DeveloperController', ['$scope', '$location', '$anchorScroll',
+                function($scope, $location, $anchorScroll) {
+                    // initialize parallax
+                    $('.parallax').parallax();
 
-				// utility function for linking between views
-				$scope.go = function (path) {
-				  $location.path(path);
-				};
+                    //initialize side nav
+                    $(".button-collapse").sideNav();
 
-				// utility function to add active css class to target element
-				$scope.activate = function(event) {
-					$(event.target).addClass('active');
-				};
+                // set page class
+                $scope.pageClass = 'developer';
 
-				// utility function to set viewport to the top of the page 
-				// on element click
-				$scope.backToTop = function() {
-					$anchorScroll();
-				};
-			}
-		]);
+                // utility function for linking between views
+                $scope.go = function(path) {
+                    $location.path(path);
+                };
+
+                // utility function to add active css class to target element
+                $scope.activate = function(event) {
+                    $(event.target).addClass('active');
+                };
+
+                // utility function to set viewport to the top of the page 
+                // on element click
+                $scope.backToTop = function() {
+                    $anchorScroll();
+                };
+            }]);
 })();
